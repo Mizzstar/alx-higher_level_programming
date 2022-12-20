7a58c8036b8af57826bb946a5465665d6a73c6f7
Mizzstar <udehesther25@gmail.com>
2022-12-20 08:26:29 -0800

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+<375@holbertonschool.com>
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __int__(self, size):
+        """Initialize a new Square.
+
+        Args:
+        size (int): The size of the 
+        new square.
+        """
+        self.__size = size
