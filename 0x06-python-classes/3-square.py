f445948cce88d37e67da10eb7e8cdd2f5de325e1
Mizzstar <udehesther25@gmail.com>
2022-12-20 23:50:59 -0800

    class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
+"""class Square"""
 
 
 class Square:
-    """Represent a square."""
-
+    """Private instance attribute and instantation with optional"""
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-        size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
-        def area(self):
-            """return the current area of the square."""
-            return (self.__size * self.__size)
+            def area(self):
+                """area of square"""
+                return self.__size ** 2
